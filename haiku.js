6b6a3ab7c9aa237821913598dadff0bcf50b7b6c
Burcu Noyan <burcunoyan@gmail.com>
2018-01-09 23:51:23 -0500

    parse dictionary and format data

--- haiku.js
+++ haiku.js
@@ -7,14 +7,28 @@ function readCmudictFile(file) {
 
 function formatData(data) {
   const lines = data.split('\n');
+  let formatted = [];
+
   lines.forEach(line => {
     const word = line.split('  ')[0];
     const phoneme = line.split('  ')[1];
-    return console.log(`the word ${word} has phoneme ${phoneme}`);
+
+    if (phoneme.match(/\d/)) {
+      const syllables = phoneme.match(/\d/g).length;
+      formatted.push({ word, phoneme, syllables });
+    }
   });
+
+  // const has5syllables = function(obj) {
+  //   return obj.syllables === 5;
+  // }
+  // console.log(arr.find(has5syllables))
+
+  return formatted;
 }
 
-function createHaiku(structure) {
+
+function createHaiku(arr, structure) {
   console.log('haiku structure will be ' + structure);
 }
 
